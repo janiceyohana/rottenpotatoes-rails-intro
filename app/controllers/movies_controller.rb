c2e1b3814c2b455d413f5c05b95760ac95478f6f
Janice Yohana <1006261@mymail.sutd.edu.sg>
2023-06-18 17:17:12 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,17 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
-  # GET /movies
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
   def index
     @all_ratings = Movie.all_ratings
-    @ratings_to_show_hash = params[:ratings] || session[:ratings] || @all_ratings.map{ |rating| [rating, "1"] }.to_h
+    @ratings_to_show_hash = params[:ratings] || session[:ratings] || @all_ratings.map {|rating| [rating, "1"]}.to_h
     @movies = Movie.with_ratings(@ratings_to_show_hash.keys)
     @sort_column = params[:sort] || session[:sort]
-    
+
     session[:sort] = @sort_column
     session[:ratings] = @ratings_to_show_hash
 
@@ -19,53 +23,38 @@ class MoviesController < ApplicationController
     end
   end
 
-  # GET /movies/1
-  def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
-  end
-
-  # GET /movies/new
   def new
+    # default: render 'new' template
   end
 
-  # GET /movies/1/edit
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
   def edit
     @movie = Movie.find params[:id]
   end
 
-  # POST /movies
-  def create
-    @movie = Movie.create!(movie_params)
-    flash[:notice] =  "#{@movie.title} was successfully created."
-    redirect_to movies_path
-    end
-  end
-  
-
-  # PATCH/PUT /movies/1
   def update
-    if @movie.update(movie_params)
-      redirect_to @movie, notice: 'Movie was successfully updated.'
-    else
-      render :edit
-    end
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
   end
 
-  # DELETE /movies/1
   def destroy
+    @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_url, notice: 'Movie was successfully destroyed.'
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def movie_params
-      params.require(:movie).permit(:title, :rating, :description, :release_date)
-    end
+  # Making "internal" methods private is not required, but is a common practice.
+  # This helps make clear which methods respond to requests, and which ones do not.
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+end
